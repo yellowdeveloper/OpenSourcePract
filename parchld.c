e5a6f183a482dad8048b1f34d5f592eccac4d0fa
LeeMinkyu <yellowdeveloper25@gmail.com>
2024-12-13 05:26:34 +0900

    first fork test

---
+++ parchld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid; /* could be int */
+    int i;
+    pid fork();
+    if (pid > 0)
+    {
+        /* parent */
+        for(i = 0; i < 1000; i++)
+            printf("\t\t\tPARENT %d\n", i);
+    }
+}
